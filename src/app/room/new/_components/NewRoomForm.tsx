5722b8e83d8b5aab47fc9932133104715c40741f
glyzinieh <contact@wisteriatp.work>
2026-02-21 14:35:41 +0900

    feat: enhance NewRoomForm with TextField component for room name input and improve layout in page

--- src/app/room/new/_components/NewRoomForm.tsx
+++ src/app/room/new/_components/NewRoomForm.tsx
@@ -1,19 +1,40 @@
 "use client";
 
+import TextField from "@/components/TextField";
 import { useState } from "react";
 import { handleFormSubmit } from "../handleFormSubmit";
 import { ImageUploader } from "./ImageUploader";
 
 export function NewRoomForm() {
+    const [roomName, setRoomName] = useState("");
     const [imagePath, setImagePath] = useState("");
 
     return (
         <>
             <form action={handleFormSubmit}>
-                <input type="text" name="roomName" placeholder="ルーム名" required />
-                <ImageUploader onImageUploaded={setImagePath} />
-                <input type="hidden" name="imagePath" value={imagePath} />
-                <button type="submit">ルームを作成</button>
+                <section className="bg-white p-5 rounded-lg shadow-md mt-10">
+                    <h2 className="text-2xl font-bold mb-5 text-[#ad3f58]">ルーム情報</h2>
+
+                    <div className="mb-4">
+                        <TextField
+                            id="roomName"
+                            name="roomName"
+                            label="ルーム名"
+                            value={roomName}
+                            onChange={(e) => setRoomName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <ImageUploader onImageUploaded={setImagePath} />
+                    <input type="hidden" name="imagePath" value={imagePath} />
+                </section>
+                <button
+                    type="submit"
+                    className="flex items-center justify-center mt-10 w-full p-5 rounded-xl bg-[linear-gradient(179deg,rgba(236,11,87,1)_49%,rgba(184,4,65,1)_100%)] hover:opacity-90 transition-opacity"
+                    aria-label="ルームを作成"
+                >
+                    <span className="text-white font-bold">ルームを作成</span>
+                </button>
             </form>
         </>
     );
--- src/app/room/new/page.tsx
+++ src/app/room/new/page.tsx
@@ -2,9 +2,12 @@ import { NewRoomForm } from "./_components/NewRoomForm";
 
 export default async function Page() {
     return (
-        <>
-            <h1>新しいルームを作成</h1>
-            <NewRoomForm />
-        </>
+        <div className="min-h-screen w-full bg-linear-to-bl from-[#F7BB97] to-[#DD5E89]">
+            <div className="p-5">
+                <h1 className="text-3xl text-white font-bold">新しいルームを作成</h1>
+                <p className="text-white mt-2">共同で編集可能なルームを作ろう！</p>
+                <NewRoomForm />
+            </div>
+        </div>
     );
 }
