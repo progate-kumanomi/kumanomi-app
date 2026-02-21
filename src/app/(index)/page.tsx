5517fad29529ca72cea0f1dbedc2930452c9708f
43mittu <43mittu@gmail.com>
2026-02-21 17:34:01 +0900

    feat: update Page component layout with logo, description, and new call-to-action button; add RightLine SVG component

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,8 +1,36 @@
+import RightLine from "@/components/RightLine";
+import { SpotAllocationStrategy } from "aws-cdk-lib/aws-autoscaling";
+
 export default async function Page() {
     return (
         <>
-            <h1>Kumanomi App</h1>
-            <a href="/room/new">つかってみる</a>
+            <div className="min-h-screen w-full bg-linear-to-bl from-[#F7BB97] to-[#DD5E89]">
+                <div className="text-white">
+                    <div className="flex">
+                        <img src="/logo.png" alt="Logo" className="w-10 h-10 m-3" />
+                        <h1 className="text-xl font-bold mt-4">ロゴタイプ</h1>
+                    </div>
+                    <p className="text-center text-4xl font-bold pt-5 mb-10">
+                        リアルタイムで、<br />
+                        みんなと一緒に編集
+                    </p>
+                    <div className="flex mt-4 items-end">
+                        <img src="/photosample.png" alt="Sample" className="w-3/5" />
+                        <div>
+                            <p className="ml-4 text-2xl pb-4">説明</p>
+                        </div>
+                    </div>
+                    <div className="px-5">
+                        <a href="/room/new" className="block mt-70 text-center text-2xl font-bold bg-white text-[#DD5E89] py-3 rounded-full w-full mx-auto">
+                            <div className="relative flex items-center justify-center">
+                                <span className="text-center">使ってみる</span>
+                                <RightLine className="absolute right-8" />
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
